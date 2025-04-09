982aaf10f61f0ec4f9958f5ba7d7c716d4b93c34
Alexey Begas <a.begas@astoundcommerce.com>
2025-04-09 12:52:45 +0200

    DNT-20: Images fix

--- src/app/treatments/page.jsx
+++ src/app/treatments/page.jsx
@@ -26,9 +26,11 @@ const Treatments = () => {
                             <h2 className="o-decor o-decor--black">{treatment.title}</h2>
                             <div className="flex flex-col mt-12">
                                 <div className="flex flex-col md:flex-row mr-10">
-                                    <div className="mb-6">
-                                        <Image src={treatment.itemImgSrc} width={600} height={600} alt={treatment.title} className="rounded-2xl brightness-75 sepia-40" />
-                                    </div>
+                                    {treatment.itemImgSrc && (
+                                        <div className="mb-6">
+                                            <Image src={treatment.itemImgSrc} width={600} height={600} alt={treatment.title} className="rounded-2xl brightness-75 sepia-40" unoptimized />
+                                        </div>
+                                    )}
                                     <div className="md:w-1/2 md:ml-8" dangerouslySetInnerHTML={{__html: treatment.text}} />
                                 </div>
                                 {treatment.text2 && <div dangerouslySetInnerHTML={{__html: treatment.text2}} />}
