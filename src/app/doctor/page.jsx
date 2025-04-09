c5125c2769ed95f07b3a38cc2d810be5280ca43f
Alexey Begas <a.begas@astoundcommerce.com>
2025-04-09 15:45:41 +0200

    DNT-24: Small fixes

--- src/app/doctor/page.jsx
+++ src/app/doctor/page.jsx
@@ -20,11 +20,11 @@ const Doctor = () => {
             />
             <ContentWithBg text={DoctorContent.text} imgSrc={DoctorContent.bgContnentImg}>
                 <div className="flex flex-col md:flex-row mt-12">
-                    <div className="md:w-1/2 mr-10">
+                    <div className="md:w-[28%] md:mr-10">
                         <Image src={DoctorContent.contentImg} width="992" height="1560" alt={DoctorContent.contentImgSubscription} className="rounded-2xl brightness-75 sepia-40" />
                         <div className="italic text-[16px] mt-4" dangerouslySetInnerHTML={{__html: DoctorContent.contentImgSubscription}} />
                     </div>                    
-                    <div className="md:w-1/2 mt-2">
+                    <div className="md:md:w-[70%] mt-2">
                         <div dangerouslySetInnerHTML={{__html: DoctorContent.textForImg}} />
                         
                     </div>
